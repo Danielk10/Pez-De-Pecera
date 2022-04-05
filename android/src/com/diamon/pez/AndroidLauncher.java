06b649b7f483fe3da523ca5e2bf6fb85b413b23f
Danielk10 <danielpdiamon@gmail.com>
2022-04-05 16:29:40 -0430

    Primer Codigo

---
+++ android/src/com/diamon/pez/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.diamon.pez;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.diamon.pez.PezDePecera;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PezDePecera(), config);
+	}
+}
