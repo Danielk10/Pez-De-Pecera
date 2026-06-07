f60cb97b7951d94ff05abf62b6f424228ca44dcf
Danielk10 <danielpdiamon@gmail.com>
2026-06-07 18:12:14 -0400

    Nuevo

--- android/src/main/java/com/diamon/pez/android/AndroidLauncher.java
+++ android/src/main/java/com/diamon/pez/android/AndroidLauncher.java
@@ -5,28 +5,17 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.diamon.pez.PezDePecera;
-
 import com.diamon.pez.publicidad.Publicidad;
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		 AppCenter.start(
-                getApplication(),
-                "4942c9c2-c670-4a16-89ab-23bfc270ca4d",
-                Analytics.class,
-                Crashes.class);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
 		
-		
-       initialize(
-                new PezDePecera(
+        initialize(new PezDePecera(
                         new Publicidad() {
 
                             @Override
@@ -58,7 +47,6 @@ public class AndroidLauncher extends AndroidApplication {
                             public void iniciarActividad() {
                                 // TODO: Implement this method
                             }
-                        }),
-                configuration);
+                        }), configuration);
     }
 }
